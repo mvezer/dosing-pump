cdc10435c9c4671ef325d8be2e1ada483c72017d
Matyas Vezer <matyas.vezer@bauerxcel.de>
2023-10-10 19:48:33 +0200

    add server

---
+++ server/go.mod
@@ -0,0 +1,38 @@
+module github.com/mvezer/dosing-pump
+
+go 1.21.0
+
+require (
+	github.com/go-playground/validator/v10 v10.15.5
+	github.com/gofiber/fiber/v2 v2.49.2
+	gobot.io/x/gobot/v2 v2.1.1
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sigurn/crc8 v0.0.0-20220107193325-2243fe600f9f // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.49.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/warthog618/gpiod v0.8.1 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	periph.io/x/conn/v3 v3.7.0 // indirect
+	periph.io/x/host/v3 v3.8.2 // indirect
+)
---
+++ server/pumpcontroller/pumpcontroller.go
@@ -0,0 +1,122 @@
+package db
+
+import (
+	"fmt"
+
+	"gobot.io/x/gobot/v2/drivers/gpio"
+	"gobot.io/x/gobot/v2/drivers/i2c"
+	"gobot.io/x/gobot/v2/platforms/raspi"
+)
+
+type Pump struct {
+	Id           int
+	Button       *gpio.ButtonDriver
+	Motor        *i2c.AdafruitMotorHatDriver
+	ButtonEvents chan interface{}
+}
+
+type PumpController struct {
+	Pumps       map[int]Pump
+	RpiAdaptor  *raspi.Adaptor
+	MotorSpeed  int32
+	MotorDriver *i2c.AdafruitMotorHatDriver
+}
+
+func Init() PumpController {
+	pc := PumpController{
+		Pumps:       make(map[int]Pump),
+		RpiAdaptor:  raspi.NewAdaptor(),
+		MotorSpeed:  128,
+		MotorDriver: i2c.NewAdafruitMotorHatDriver(raspi.NewAdaptor()),
+	}
+	pc.MotorDriver.Start()
+
+	return pc
+}
+
+func (pc *PumpController) Start() {
+	pc.MotorDriver.Start()
+}
+
+func (pc *PumpController) AddPump(id int, buttonPin string) {
+	pc.Pumps[id] = Pump{
+		Id:     id,
+		Button: gpio.NewButtonDriver(pc.RpiAdaptor, buttonPin),
+	}
+	pc.Pumps[id].Button.Start()
+	pc.Pumps[id].Button.On(gpio.ButtonPush, func(data interface{}) {
+		fmt.Printf("Pump %d button pushed\n", id)
+		pc.RunPump(id, 0)
+	})
+	pc.Pumps[id].Button.On(gpio.ButtonRelease, func(data interface{}) {
+		fmt.Printf("Pump %d button released\n", id)
+		pc.StopPump(id)
+	})
+	pc.Pumps[id].Motor.Start()
+}
+
+func (pc *PumpController) RunPump(id int, interval int) {
+	fmt.Printf("Running pump %d\n", id)
+	if err := pc.MotorDriver.SetDCMotorSpeed(id, pc.MotorSpeed); err != nil {
+		fmt.Printf("Error setting motor speed: %s\n", err)
+	}
+	if err := pc.MotorDriver.RunDCMotor(id, i2c.AdafruitForward); err != nil {
+		fmt.Printf("Error running motor: %s\n", err)
+	}
+}
+
+func (pc *PumpController) StopPump(id int) {
+	fmt.Printf("Stopping pump %d\n", id)
+	if err := pc.MotorDriver.RunDCMotor(id, i2c.AdafruitRelease); err != nil {
+		fmt.Printf("Error stopping the motor: %s\n", err)
+	}
+}
+
+// func adafruitDCMotorRunner(a *i2c.AdafruitMotorHatDriver, dcMotor int) (err error) {
+// 	log.Printf("DC Motor Run Loop...\n")
+// 	// set the speed:
+// 	var speed int32 = 255 // 255 = full speed!
+// 	if err = a.SetDCMotorSpeed(dcMotor, speed); err != nil {
+// 		return
+// 	}
+// 	// run FORWARD
+// 	if err = a.RunDCMotor(dcMotor, i2c.AdafruitForward); err != nil {
+// 		return
+// 	}
+// 	// Sleep and RELEASE
+// 	time.Sleep(2000 * time.Millisecond)
+// 	if err = a.RunDCMotor(dcMotor, i2c.AdafruitRelease); err != nil {
+// 		return
+// 	}
+// 	// run BACKWARD
+// 	if err = a.RunDCMotor(dcMotor, i2c.AdafruitBackward); err != nil {
+// 		return
+// 	}
+// 	// Sleep and RELEASE
+// 	time.Sleep(2000 * time.Millisecond)
+// 	if err = a.RunDCMotor(dcMotor, i2c.AdafruitRelease); err != nil {
+// 		return
+// 	}
+// 	return
+// }
+//
+// func InitMotorController() {
+// 	r := raspi.NewAdaptor()
+// 	adaFruit := i2c.NewAdafruitMotorHatDriver(r)
+//
+// 	work := func() {
+// 		gobot.Every(5*time.Second, func() {
+//
+// 			dcMotor := 2 // 0-based
+// 			adafruitDCMotorRunner(adaFruit, dcMotor)
+// 		})
+// 	}
+//
+// 	robot := gobot.NewRobot("adaFruitBot",
+// 		[]gobot.Connection{r},
+// 		[]gobot.Device{adaFruit},
+// 		work,
+// 	)
+//
+// 	robot.Start()
+// }
